12da63d2153ec622c9e1e376a4013d8a08c2aa58
jpt4 <jpt4@proflog.dev>
2026-04-19 04:05:45 -0400

    Add recursive parity witness synthesis tests

---
+++ test/proflog/recursive_synthesis_test.clj
@@ -0,0 +1,74 @@
+(ns proflog.recursive-synthesis-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [proflog.ast :as ast]
+            [proflog.language :as language]
+            [proflog.query :as query]))
+
+(def parity-synthesis-language
+  (language/language
+    {:constants ['zero]
+     :functions {'s 1}
+     :relations {'even 1
+                 'odd 1}}))
+
+(defn numeral
+  [n]
+  (if (zero? n)
+    (ast/app-term 'zero)
+    (ast/app-term 's (numeral (dec n)))))
+
+(defn recursive-parity-program
+  []
+    (ast/nom x y z
+    (language/compile-program
+      parity-synthesis-language
+      [(ast/clause 'even [x]
+                   (ast/or-form
+                     (ast/eq-lit (ast/var-term x) (ast/app-term 'zero))
+                     (ast/exists-form y
+                                      (ast/and-form
+                                        (ast/eq-lit (ast/var-term x)
+                                                    (ast/app-term 's (ast/var-term y)))
+                                        (ast/pos-lit (ast/app-term 'odd (ast/var-term y)))))))
+       (ast/clause 'odd [x]
+                   (ast/exists-form z
+                                    (ast/and-form
+                                      (ast/eq-lit (ast/var-term x)
+                                                  (ast/app-term 's (ast/var-term z)))
+                                      (ast/pos-lit (ast/app-term 'even (ast/var-term z))))))])))
+
+(defn formula-succeeds-directly?
+  [program formula fuel]
+  (seq (query/query-succeeds program formula 1 fuel)))
+
+(defn successful-witnesses
+  [program formula-builder witnesses fuel]
+  ;; Keep synthesis probes on the positive side of the semidecision boundary:
+  ;; unrestricted open-answer search is still too operationally unstable for a
+  ;; regression test, but expected witnesses can be checked directly.
+  (->> witnesses
+       (keep #(when (formula-succeeds-directly? program (formula-builder %) fuel)
+                %))
+       vec))
+
+(deftest recursive-parity-witness-enumeration-finds-even-numerals
+  (testing "positive witness enumeration collects multiple even numerals"
+    (let [program (recursive-parity-program)
+          witnesses [(numeral 0) (numeral 2)]]
+      (is (= witnesses
+             (successful-witnesses
+               program
+               #(ast/pos-lit (ast/app-term 'even %))
+               witnesses
+               8))))))
+
+(deftest recursive-parity-witness-enumeration-finds-odd-numerals
+  (testing "positive witness enumeration collects multiple odd numerals"
+    (let [program (recursive-parity-program)
+          witnesses [(numeral 1) (numeral 3)]]
+      (is (= witnesses
+             (successful-witnesses
+               program
+               #(ast/pos-lit (ast/app-term 'odd %))
+               witnesses
+               8))))))
